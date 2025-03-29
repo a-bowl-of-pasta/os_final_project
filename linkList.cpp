dac72191d5b9d9904e0edecc6d53640756cc01b1
bowlOfPasta <ahernandez94476@gmail.com>
2025-03-29 16:26:23 -0700

    os_final v0.0
    created initial files; made template for linked list;

---
+++ linkList.cpp
@@ -0,0 +1,37 @@
+class LinkedList
+{
+	private:
+	
+		class llnode()
+		{
+			int data; 
+			llnode* next; 
+			llnode* previous;
+		};
+
+		llnode* head; 
+		llnode* tail;
+	public:
+	
+
+	void insert()
+	{
+	}
+
+	void remove() 
+	{
+	}
+
+	void peek()
+	{
+	}
+
+	void print()
+	{
+	}
+	
+	void findNode()
+	{
+	}
+
+};
