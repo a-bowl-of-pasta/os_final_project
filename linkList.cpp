75e19168c35fb50c9c2b676588e9c508e3f303a1
bowlOfPasta <ahernandez94476@gmail.com>
2025-04-02 13:14:38 -0700

    osProj v2.0
    nearly complete linked list

--- linkList.cpp
+++ linkList.cpp
@@ -1,37 +1,81 @@
 class LinkedList
 {
-	private:
+		private:
 	
-		class llnode()
+		class llnode
 		{
+			public: 
+			llnode(){}
 			int data; 
 			llnode* next; 
 			llnode* previous;
 		};
 
+		int nodeCount; 
 		llnode* head; 
 		llnode* tail;
-	public:
 	
-
-	void insert()
-	{
-	}
-
-	void remove() 
-	{
-	}
-
-	void peek()
-	{
-	}
-
-	void print()
-	{
-	}
+		public:
 	
-	void findNode()
-	{
-	}
+		LinkedList()
+		{
+			int nodeCount = 0; 
+			head = nullptr;
+			tail = nullptr; 
+		}
 
+		// ---- insert at end
+		void insert(int num)
+		{
+			llnode* newNode = new llnode(); 
+
+			if (nodeCount == 0) 
+			{
+				head = newNode; 
+				tail = newNode; 
+				nodeCount++; 
+			}
+			else
+			{
+				tail -> next = newNode; 
+				newNode -> previous = tail; 
+				
+				tail = newNode; 
+
+				tail -> next = head; 
+				head -> previous = tail; 
+			}
+		}
+		// ------- remove last node
+		void removeEnd() 
+		{
+			tail = tail->previous; 
+			delete tail->next; 
+		
+			tail -> next = head; 
+			head -> previous = tail; 
+
+			nodeCount--;   
+		}
+		// ------- view data at first node
+		int peek()
+		{
+			return head->data; 
+		}
+		// ------- print entire list
+		void print()
+		{
+			/*
+			llnode* travelNode = head;
+			for(int i =0; i < nodeCount; i++)
+			{
+				std::cout << travelNode->data << std::endl; 
+				travelNode = travelNode->next; 
+			}
+			*/
+		}
+		// ------- find specific node
+		void findNode(int num)
+		{
+		}
 };
